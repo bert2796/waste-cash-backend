28b0505e24e1d6b04bb9cde293c4cf352c3bd874
Albert Nakpil <a.nakpil@archax.com>
2021-12-10 08:46:29 +0800

    fix string notation

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserRepository } from './repositories/user.repository';
-import { UserController } from "./controllers/user.controller";
+import { UserController } from './controllers/user.controller';
 import { UserService } from './services/user.service';
 
 @Module({
